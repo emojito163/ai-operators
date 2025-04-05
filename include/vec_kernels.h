6aeda1f575ccadc0c8d75d0f0df837e3e41889a8
emojito163 <emojito@163.com>
2025-04-05 16:53:20 +0800

    Complete vec_div_kernel;

--- include/vec_kernels.h
+++ include/vec_kernels.h
@@ -70,6 +70,27 @@ inline void vec_mul_kernel(int8_t* a, int8_t* b, int8_t* c, ::std::size_t num) n
 namespace details {
 
 template<::std::size_t ProcNum>
+__global__ void vec_div_kernel_(int8_t* a, int8_t* b, int8_t* c, ::std::size_t num) {
+    int num_per_proc = num / ProcNum;
+    int proc_id = GetProcId();
+    int offset = proc_id * num_per_proc;
+
+    for (int i = 0; i < num_per_proc; ++i) {
+        c[offset + i] = a[offset + i] / b[offset + i];
+    }
+    RISCV_FENCE_I;  // Flush cache
+}
+
+} // namespace details
+
+template<::std::size_t Kernel, ::std::size_t Tier>
+inline void vec_div_kernel(int8_t* a, int8_t* b, int8_t* c, ::std::size_t num) noexcept {
+    ::details::vec_div_kernel_<Kernel * Tier><<<Kernel, Tier>>>(a, b, c, num);
+}
+
+namespace details {
+
+template<::std::size_t ProcNum>
 __global__ void vec_abs_kernel_(int8_t* a, int8_t* b, ::std::size_t num) {
     int num_per_proc = num / ProcNum;
     int proc_id = GetProcId();
